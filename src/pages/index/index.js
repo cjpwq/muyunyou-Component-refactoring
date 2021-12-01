29589cb3e0cefb04318f3235c2f06616042dc419
cjpwq <50103094+cjpwq@users.noreply.github.com>
2021-12-01 10:28:00 +0800

    幻灯片组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,8 +4,9 @@ import './css/bundle.css';
 
 // js
 import './js/menu';
-import './js/carousel';
+// import './js/carousel';
 import './js/backtotop';
 
 // 组件
 import 'components/topbar';
+import './components/carousel';
